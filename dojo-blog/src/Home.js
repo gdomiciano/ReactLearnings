3f54bc6e736eb03a39051753dfe9fa1a05afb38a
Geisy Domiciano <geisydomiciano@gmail.com>
2025-03-25 15:26:46 +0100

    :construction: Lesson 10: outputting lists

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,26 +1,21 @@
 import { useState } from 'react';
 
 const Home = () => {
-  const [name, setName] = useState('mario')
-  const [age, setAge] = useState(25)
-
-  const handleClick = (e) => {
-    setName('luigi');
-    setAge(30);
-  }
-
-  const handleClickWithParams = (name) => console.log('hello, ' + name)
-  const handleClickWithParamsAndEvent = (name, e) => console.log('hello, ' + name, e.target)
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
 
   return (
-    <div className="home">
-      <h2>Homepage</h2>
-      <p>{name} is {age}</p>
-      <button onClick={handleClick}>click me</button>
-
-      <button onClick={() => handleClickWithParams('test')}>click me with params</button>
-      <button onClick={(e) => handleClickWithParamsAndEvent('test2', e)}>click me with params and event</button>
-    </div>
+    <section className="home">
+      {blogs.map((blog) => (
+        <article className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>{blog.author}</p>
+        </article>
+      ))}
+    </section>
    );
 }
 

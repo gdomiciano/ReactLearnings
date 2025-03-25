55b755b9ed7e45d664d087d00fe0b977e07d449a
Geisy Domiciano <geisydomiciano@gmail.com>
2025-03-25 14:40:36 +0100

    :construction: Lesson 4: Dynamic values in templates

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 
 function App() {
+
+  const title  = 'Welcome to the new blog';
+  const likes = 50;
+  const bDayInArr = [1993, 3, 26]
+  const url = 'https://www.google.com'
+
   return (
     <div className="App">
       <div className="content">
-        <h1>AppComponent</h1>
+        <h1>{title}</h1>
+        <p>liked {likes} times</p>
+        <p>{bDayInArr}</p>
+        <p>{Math.random() * 10}</p>
+        <a href={url}>Google</a>
       </div>
     </div>
   );

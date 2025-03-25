99d628c6afeba7c50cb2bcc594ba9cf3d4202b5e
Geisy Domiciano <geisydomiciano@gmail.com>
2025-03-25 14:47:23 +0100

    :construction: Lesson 5: multiple components

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
 
@@ -9,13 +11,10 @@ function App() {
 
   return (
     <div className="App">
-      <div className="content">
-        <h1>{title}</h1>
-        <p>liked {likes} times</p>
-        <p>{bDayInArr}</p>
-        <p>{Math.random() * 10}</p>
-        <a href={url}>Google</a>
-      </div>
+      <Navbar />
+      <main className="content">
+        <Home />
+      </main>
     </div>
   );
 }
---
+++ dojo-blog/src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <aside className="navbar">
+      <h1>The Dojo Blog</h1>
+      <nav className="links">
+        <a href="/">home</a>
+        <a href="/create">New Blog Post</a>
+      </nav>
+    </aside>
+   );
+}
+
+export default Navbar;

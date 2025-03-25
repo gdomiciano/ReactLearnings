a041e455c0c6968c604046d3c437a90da50e3d73
Geisy Domiciano <geisydomiciano@gmail.com>
2025-03-25 15:10:14 +0100

    :construction: Lesson 8: using state (useState hook)

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,13 +1,18 @@
-const Home = () => {
+import { useState } from 'react';
 
-  const handleClick = (e) => console.log('hello, ninjas', e)
+const Home = () => {
+const [name, setName] = useState('mario')
+  const handleClick = (e) => setName('luigi')
+
   const handleClickWithParams = (name) => console.log('hello, ' + name)
   const handleClickWithParamsAndEvent = (name, e) => console.log('hello, ' + name, e.target)
 
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>{name}</p>
       <button onClick={handleClick}>click me</button>
+
       <button onClick={() => handleClickWithParams('test')}>click me with params</button>
       <button onClick={(e) => handleClickWithParamsAndEvent('test2', e)}>click me with params and event</button>
     </div>

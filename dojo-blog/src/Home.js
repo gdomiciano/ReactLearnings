7a86ae74266347fa5a5adad5e520cb80890c3f20
Geisy Domiciano <geisydomiciano@gmail.com>
2025-03-25 15:18:40 +0100

    :construction: Lesson 9: updates to visualize in the extension tool

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,8 +1,13 @@
 import { useState } from 'react';
 
 const Home = () => {
-const [name, setName] = useState('mario')
-  const handleClick = (e) => setName('luigi')
+  const [name, setName] = useState('mario')
+  const [age, setAge] = useState(25)
+
+  const handleClick = (e) => {
+    setName('luigi');
+    setAge(30);
+  }
 
   const handleClickWithParams = (name) => console.log('hello, ' + name)
   const handleClickWithParamsAndEvent = (name, e) => console.log('hello, ' + name, e.target)
@@ -10,7 +15,7 @@ const [name, setName] = useState('mario')
   return (
     <div className="home">
       <h2>Homepage</h2>
-      <p>{name}</p>
+      <p>{name} is {age}</p>
       <button onClick={handleClick}>click me</button>
 
       <button onClick={() => handleClickWithParams('test')}>click me with params</button>

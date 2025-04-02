5c2b23610a43b07062aee83d411409ae69abc5db
Geisy Domiciano <geisydomiciano@gmail.com>
2025-04-02 15:03:09 +0200

    :construction: Lesson 12: Reusing Components

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -11,6 +11,7 @@ const Home = () => {
   return (
     <section className="home">
       <BlogList blogs={blogs} title="All Blogs"/>
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario' )} title="Mario's Blogs"/>
     </section>
    );
 }

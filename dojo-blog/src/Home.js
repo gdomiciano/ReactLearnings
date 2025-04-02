1d62bc17f73bf60ebb39e8938ef45eec0bcd1246
Geisy Domiciano <geisydomiciano@gmail.com>
2025-04-02 15:41:19 +0200

    :construction: Lesson 16 . add json server

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -7,7 +7,6 @@ const Home = () => {
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
-  const [name, setName] = useState('mario')
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -15,15 +14,13 @@ const Home = () => {
   }
 
   useEffect(() => {
-    console.log('useEffect ran', blogs, name)
-  },[name]);
+    console.log('useEffect ran', blogs)
+  },[]);
 
   return (
     <section className="home">
       <BlogList blogs={blogs} handleDelete={handleDelete} title="All Blogs"/>
       <BlogList blogs={blogs.filter((blog) => blog.author === 'mario' )} title="Mario's Blogs"/>
-      <button onClick={()=> setName('Luigi')}>Change name</button>
-      <p>{name}</p>
     </section>
    );
 }

32c9f54f73bde0a793a89249f0aaf586fb2cf2c1
Geisy Domiciano <geisydomiciano@gmail.com>
2025-04-02 15:49:44 +0200

    :construction: Lesson 17 fetching data with useEffect

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,25 +2,22 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
+  const [blogs, setBlogs] = useState(null)
 
   useEffect(() => {
     console.log('useEffect ran', blogs)
-  },[]);
+    fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        setBlogs(data)
+      })
+  }, []);
 
   return (
     <section className="home">
-      <BlogList blogs={blogs} handleDelete={handleDelete} title="All Blogs"/>
-      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario' )} title="Mario's Blogs"/>
+      {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
     </section>
    );
 }

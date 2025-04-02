698ccd93cb1ccec8bea2544e199b08d47a77874d
Geisy Domiciano <geisydomiciano@gmail.com>
2025-04-02 18:42:47 +0200

    :construction: lesson 22: exact match rules

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,21 +1,20 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import Create from './Create';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 function App() {
-
-  const title  = 'Welcome to the new blog';
-  const likes = 50;
-  const bDayInArr = [1993, 3, 26]
-  const url = 'https://www.google.com'
-
   return (
     <Router>
       <div className="App">
         <Navbar />
         <main className="content">
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
               <Home />
+            </Route>
+            <Route exact path="/create">
+              <Create />
             </Route>
           </Switch>
         </main>

103291f17bd35e18eb2d5c335521d63df9be091e
Geisy Domiciano <geisydomiciano@gmail.com>
2025-04-02 15:32:35 +0200

    :construction: Lesson 15 - useEffect dependencies

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -7,17 +7,23 @@ const Home = () => {
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
+  const [name, setName] = useState('mario')
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   }
 
-  useEffect(() => {console.log('useEffect ran', blogs)});
+  useEffect(() => {
+    console.log('useEffect ran', blogs, name)
+  },[name]);
+
   return (
     <section className="home">
       <BlogList blogs={blogs} handleDelete={handleDelete} title="All Blogs"/>
       <BlogList blogs={blogs.filter((blog) => blog.author === 'mario' )} title="Mario's Blogs"/>
+      <button onClick={()=> setName('Luigi')}>Change name</button>
+      <p>{name}</p>
     </section>
    );
 }

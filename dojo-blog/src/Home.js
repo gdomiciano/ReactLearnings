551e30c74a105121cd6253927b86deb4804ea5a7
Geisy Domiciano <geisydomiciano@gmail.com>
2025-04-02 18:06:03 +0200

    :construction: lesson 20: making a custom hook

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,33 +1,13 @@
-import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null)
-  const [isPending, setIsPending] = useState(true)
-  const [error, setError] = useState(null)
 
-  useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then(res => {
-        if (!res.ok) {
-          throw Error('Could not fetch the data for that resource')
-        }
-        return res.json()
-      })
-      .then(data => {
-        setBlogs(data)
-        setIsPending(false)
-        setError(null)
-      }).catch(err => {
-        setError(err.message)
-        setIsPending(false)
-      })
-  }, []);
-
+const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
   return (
     <section className="home">
       {error && <div>{error}</div>}
-      { isPending && <div>Loading...</div> }
+      {isPending && <div>Loading...</div> }
       {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
     </section>
    );

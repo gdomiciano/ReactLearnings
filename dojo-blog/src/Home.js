b2784fbd4b3730cca5f3aef120a478aa244401a1
Geisy Domiciano <geisydomiciano@gmail.com>
2025-04-02 15:53:00 +0200

    :construction: Lesson 18: conditional loading message

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -3,7 +3,7 @@ import BlogList from './BlogList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null)
-
+  const [isPending, setIsPending] = useState(true)
   useEffect(() => {
     console.log('useEffect ran', blogs)
     fetch('http://localhost:8000/blogs')
@@ -12,11 +12,13 @@ const Home = () => {
       })
       .then(data => {
         setBlogs(data)
+        setIsPending(false)
       })
   }, []);
 
   return (
     <section className="home">
+      { isPending && <div>Loading...</div> }
       {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
     </section>
    );

b9e413d7f0b60ef84605f673e679a1f2512302fb
Geisy Domiciano <geisydomiciano@gmail.com>
2025-04-08 12:22:14 +0200

    :construction: lesson 30: programmatic redirects

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,14 +1,28 @@
 import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
+  const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
-    console.log(blog);
+
+    setIsPending(true);
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log('New blog added');
+      setIsPending(false);
+      history.push('/');
+    })
   }
 
     return (
@@ -36,7 +50,8 @@ const Create = () => {
           <option value="yoshi">Yoshi</option>
           <option value="luigi">Luigi</option>
         </select>
-        <button>Add Blog</button>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding Blog...</button>}
       </form>
     </div>
    );

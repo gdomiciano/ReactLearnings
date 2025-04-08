8bc819c73c94eace201847dbffa984e9ac70344b
Geisy Domiciano <geisydomiciano@gmail.com>
2025-04-08 11:22:25 +0200

    :construction: Lesson 23: Router Links

--- dojo-blog/src/Navbar.js
+++ dojo-blog/src/Navbar.js
@@ -1,10 +1,12 @@
+import {Link} from 'react-router-dom';
+
 const Navbar = () => {
   return (
     <aside className="navbar">
       <h1>The Dojo Blog</h1>
       <nav className="links">
-        <a href="/">home</a>
-        <a href="/create" >New Blog Post</a>
+        <Link to="/">home</Link>
+        <Link to="/create" >New Blog Post</Link>
       </nav>
     </aside>
    );

b6e410e026ebff9fb81c3581ef1797c72692dd66
Geisy Domiciano <geisydomiciano@gmail.com>
2025-04-08 12:36:58 +0200

    :sparkles: add not found and finalize the course

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,8 +1,9 @@
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
             </Route>
             <Route exact path="/blogs/:id">
               <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </main>

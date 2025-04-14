1d4110756d6dbe9dd24a10bce6b1a1849b1b9da8
Geisy Domiciano <geisydomiciano@gmail.com>
2025-04-14 15:14:56 +0200

    :lipstick: styling the components

--- globomantics/src/App.jsx
+++ globomantics/src/App.jsx
@@ -1,5 +1,6 @@
 
 import Banner from './components/Banner'
+import './App.css'
 
 function App() {
    return (
--- globomantics/src/components/Banner.jsx
+++ globomantics/src/components/Banner.jsx
@@ -1,9 +1,11 @@
 import logo from '../assets/GloboLogo.png';
+import {logo as logoClass, subtitle} from './Banner.module.css';
+
 const Banner = () => {
   return (
-    <header>
-      <div><img src={logo} alt="logo" /></div>
-      <div>Providing houses all over the world!</div>
+    <header className="row mb-4">
+      <div className='col-5'><img src={logo} className={logoClass} alt="logo" /></div>
+      <div className={`col-7 ${subtitle}`}>Providing houses all over the world!</div>
     </header>
    );
 }
